589de885f26ac0baa23f83cede43c126829aaad0
unknown <suyao@wbj-suyao18.gd.ctc.com>
2018-08-31 09:52:18 +0800

    2day-2 commit

--- blueking/component/apis/cc.py
+++ blueking/component/apis/cc.py
@@ -10,43 +10,15 @@ class CollectionsCC(object):
 
         self.add_plat_id = ComponentAPI(
             client=self.client, method='POST', path='/api/c/compapi/cc/add_plat_id/',
-            description=u'新增子网ID',
-        )
-        self.add_set = ComponentAPI(
-            client=self.client, method='POST', path='/api/c/compapi/cc/add_set/',
-            description=u'新建集群',
-        )
-        self.clone_host_property = ComponentAPI(
-            client=self.client, method='POST', path='/api/c/compapi/cc/clone_host_property/',
-            description=u'克隆主机属性',
+            description=u'新增平台 ID',
         )
         self.del_plat = ComponentAPI(
             client=self.client, method='POST', path='/api/c/compapi/cc/del_plat/',
-            description=u'删除子网',
-        )
-        self.del_set = ComponentAPI(
-            client=self.client, method='POST', path='/api/c/compapi/cc/del_set/',
-            description=u'删除集群',
-        )
-        self.del_set_host = ComponentAPI(
-            client=self.client, method='POST', path='/api/c/compapi/cc/del_set_host/',
-            description=u'清空集群主机',
-        )
-        self.get_app_agent_status = ComponentAPI(
-            client=self.client, method='GET', path='/api/c/compapi/cc/get_app_agent_status/',
-            description=u'查询业务下Agent状态',
-        )
-        self.get_app_by_id = ComponentAPI(
-            client=self.client, method='GET', path='/api/c/compapi/cc/get_app_by_id/',
-            description=u'查询业务信息',
+            description=u'删除平台',
         )
         self.get_app_by_user = ComponentAPI(
             client=self.client, method='GET', path='/api/c/compapi/cc/get_app_by_user/',
             description=u'查询用户有权限的业务',
-        )
-        self.get_app_by_user_role = ComponentAPI(
-            client=self.client, method='GET', path='/api/c/compapi/cc/get_app_by_user_role/',
-            description=u'根据用户角色查询用户业务',
         )
         self.get_app_host_list = ComponentAPI(
             client=self.client, method='GET', path='/api/c/compapi/cc/get_app_host_list/',
@@ -55,18 +27,6 @@ class CollectionsCC(object):
         self.get_app_list = ComponentAPI(
             client=self.client, method='GET', path='/api/c/compapi/cc/get_app_list/',
             description=u'查询业务列表',
-        )
-        self.get_host_by_company_id = ComponentAPI(
-            client=self.client, method='GET', path='/api/c/compapi/cc/get_host_by_company_id/',
-            description=u'根据开发商ID、子网ID、主机IP获取主机信息',
-        )
-        self.get_host_company_id = ComponentAPI(
-            client=self.client, method='GET', path='/api/c/compapi/cc/get_host_company_id/',
-            description=u'获取主机开发商',
-        )
-        self.get_host_list_by_field = ComponentAPI(
-            client=self.client, method='GET', path='/api/c/compapi/cc/get_host_list_by_field/',
-            description=u'根据主机属性的值group主机列表',
         )
         self.get_host_list_by_ip = ComponentAPI(
             client=self.client, method='GET', path='/api/c/compapi/cc/get_host_list_by_ip/',
@@ -85,7 +45,7 @@ class CollectionsCC(object):
             description=u'查询模块主机列表',
         )
         self.get_modules = ComponentAPI(
-            client=self.client, method='GET', path='/api/c/compapi/cc/get_modules/',
+            client=self.client, method='POST', path='/api/c/compapi/cc/get_modules/',
             description=u'查询业务下的所有模块',
         )
         self.get_modules_by_property = ComponentAPI(
@@ -94,15 +54,7 @@ class CollectionsCC(object):
         )
         self.get_plat_id = ComponentAPI(
             client=self.client, method='GET', path='/api/c/compapi/cc/get_plat_id/',
-            description=u'查询子网列表',
-        )
-        self.get_process_port_by_app_id = ComponentAPI(
-            client=self.client, method='GET', path='/api/c/compapi/cc/get_process_port_by_app_id/',
-            description=u'查询进程端口',
-        )
-        self.get_property_list = ComponentAPI(
-            client=self.client, method='GET', path='/api/c/compapi/cc/get_property_list/',
-            description=u'查询属性列表',
+            description=u'查询平台列表',
         )
         self.get_set_host_list = ComponentAPI(
             client=self.client, method='GET', path='/api/c/compapi/cc/get_set_host_list/',
@@ -120,10 +72,6 @@ class CollectionsCC(object):
             client=self.client, method='GET', path='/api/c/compapi/cc/get_topo_tree_by_app_id/',
             description=u'查询业务拓扑树',
         )
-        self.update_custom_property = ComponentAPI(
-            client=self.client, method='POST', path='/api/c/compapi/cc/update_custom_property/',
-            description=u'更新集群属性',
-        )
         self.update_gse_proxy_status = ComponentAPI(
             client=self.client, method='POST', path='/api/c/compapi/cc/update_gse_proxy_status/',
             description=u'更新主机gse agent proxy 状态',
@@ -132,23 +80,7 @@ class CollectionsCC(object):
             client=self.client, method='POST', path='/api/c/compapi/cc/update_host_by_app_id/',
             description=u'更新主机的gse agent状态',
         )
-        self.update_host_module = ComponentAPI(
-            client=self.client, method='POST', path='/api/c/compapi/cc/update_host_module/',
-            description=u'修改主机模块',
-        )
         self.update_host_plat = ComponentAPI(
             client=self.client, method='POST', path='/api/c/compapi/cc/update_host_plat/',
-            description=u'更新主机云子网',
-        )
-        self.update_module_property = ComponentAPI(
-            client=self.client, method='POST', path='/api/c/compapi/cc/update_module_property/',
-            description=u'更新模块属性',
-        )
-        self.update_set_property = ComponentAPI(
-            client=self.client, method='POST', path='/api/c/compapi/cc/update_set_property/',
-            description=u'更新集群属性',
-        )
-        self.update_set_service_status = ComponentAPI(
-            client=self.client, method='POST', path='/api/c/compapi/cc/update_set_service_status/',
-            description=u'更新集群状态',
+            description=u'更新主机云平台',
         )
